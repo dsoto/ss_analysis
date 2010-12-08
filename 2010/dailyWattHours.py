7540b7509da0cd1c8c88de1bbef780327157eed0
Daniel Soto <danielrsoto@gmail.com>
2010-12-08 10:35:43 +0000

    added looping to file and cleaner output
    still lacks way to have very clean excel ready output

--- 2010/dailyWattHours.py
+++ 2010/dailyWattHours.py
@@ -1,51 +1,80 @@
+# script to scrape log files for daily use
 
 import os    
 import numpy as np
 
-verbose = 1
+# debug flag
+verbose = 0
 
-outputFileList = []
-outputFileDict = {}
+def printDate(date):
+    outputFileList = []
+    outputFileDict = {}
+    wattHourDict = {}
 
-# generate list of log files and find last hour of logging
-for dirname, dirnames, filenames in os.walk('12/01'):
-    if verbose == 1:
-        print
-        print dirname
-        print dirnames
-        print filenames
-    if filenames == []:
+    # generate list of log files and find last hour of logging
+    for dirname, dirnames, filenames in os.walk(date):
         if verbose == 1:
-            print 'no files in this directory'
-    else:
-        for filename in filenames:
-            if filename not in outputFileList:
-                outputFileList.append(filename)
-            outputFileDict[filename] = dirname
-
-# sort keys and output information
-keys = outputFileDict.keys()
-keys.sort()
-
-for key in keys:
-    print
-    print key, outputFileDict[key]
+            print
+            print dirname
+            print dirnames
+            print filenames
+        if filenames == []:
+            if verbose == 1:
+                print 'no files in this directory'
+        else:
+            for filename in filenames:
+                if filename not in outputFileList:
+                    outputFileList.append(filename)
+                outputFileDict[filename] = dirname
     
-    # open file in appropriate directory and pull last line
-    filename = outputFileDict[key] + '/' + key
+    # sort keys and output information
+    keys = outputFileDict.keys()
+    keys.sort()
+    
+    for key in keys:
+        if verbose == 1:
+            print
+            print key, outputFileDict[key]
+        
+        # open file in appropriate directory and pull last line
+        filename = outputFileDict[key] + '/' + key
+    
+        # get appropriate column of data
+        totalWattHourCol = 6
+        # watch out for pesky mains file '200'
+        if '200' in key:
+            totalWattHourCol = 5
+        data = np.loadtxt(filename, delimiter=',', 
+                                    usecols = (0,totalWattHourCol), 
+                                    skiprows=1)
+        # some of these log files only have one line of data
+        if data.shape == (2,):
+            timeStamp = int(data[0])
+            totalWattHour = data[1]
+        else:
+            timeStamp = int(data[-1,0])
+            totalWattHour = data[-1,1]
+        
+        if verbose == 1:
+            print timeStamp, 'time of last log'
+            print totalWattHour, 'watt-hours'
+        
+        wattHourDict[key] = totalWattHour
+    
+    # output lines for spreadsheet
+    
+    print
+    print date    
+    for key in keys:
+        print key[-7:-4],
+    print
+    for key in keys:
+        print wattHourDict[key],
+    print
+    
 
-    # get appropriate column of data
-    totalWattHourCol = 6
-    # watch out for pesky mains file '200'
-    if '200' in key:
-        totalWattHourCol = 5
-    data = np.loadtxt(filename, delimiter=',', 
-                                usecols = (0,totalWattHourCol), 
-                                skiprows=1)
-    # some of these log files only have one line of data
-    if data.shape == (2,):
-        print int(data[0])
-        print data[1], 'watt-hours'
-    else:
-        print int(data[-1,0])
-        print data[-1,1], 'watt-hours'
+# main loop
+dateList = ['12/02','12/03','12/04','12/05','12/06']
+
+for date in dateList:
+    printDate(date)
