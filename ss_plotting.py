1a116a8db298c5d8a723ac384a22c298393a3886
Daniel Soto <danielrsoto@gmail.com>
2010-12-31 09:44:35 -0500

    improve handling of one-line data files for both circuit and mains files

--- ss_plotting.py
+++ ss_plotting.py
@@ -24,10 +24,12 @@ def getData(plotCircuit, plotDate, downsample, dataDirectory):
                 if '200' in plotCircuit:
                     usecols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
                                11, 12, 13, 14, 15, 16, 17, 18]
+                    numColumns = len(usecols)
                 else:                        
                      usecols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
                                 11, 12, 13, 14, 15, 16, 17, 18, 20]
                 tempData = np.loadtxt(dirname+'/'+filename, 
+                    numColumns = len(usecols)
                                       usecols = usecols,
                                       delimiter=',', 
                                       skiprows = 1)
