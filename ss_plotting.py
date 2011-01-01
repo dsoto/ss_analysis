3e51235542c9c0c962359509305f16126ce58246
Daniel Soto <danielrsoto@gmail.com>
2011-01-01 17:08:32 -0500

    adding data reader with more formatting brains

--- ss_plotting.py
+++ ss_plotting.py
@@ -1,9 +1,100 @@
 verbose = 0
 import os
 import numpy as np
+import dateutil.parser
+import matplotlib.dates
+import datetime
 numColumns = 20
 
 
+def getFormattedData(circuit, beginDatetime, endDatetime, downsample, dataDirectory):
+    # get formats together for arrays
+    namesCircuit = ['Time Stamp',
+                'Watts',
+                'Volts',
+                'Amps',
+                'Watt Hours SC20',
+                'Watt Hours Today',
+                'Max Watts',
+                'Max Volts',
+                'Max Amps',
+                'Min Watts',
+                'Min Volts',
+                'Min Amps',
+                'Power Factor',
+                'Power Cycle',
+                'Frequency',
+                'Volt Amps',
+                'Relay Not Closed',
+                'Send Rate',
+                'Machine ID',
+                'Type',
+                'Credit']    
+    namesMains = namesCircuit[0:-1]
+    formatsCircuit = ['S14',
+                  'float',
+                  'float',
+                  'float',
+                  'float',
+                  'float',
+                  'float',
+                  'float',
+                  'float',
+                  'float',
+                  'float',
+                  'float',
+                  'float',
+                  'float',
+                  'float',
+                  'float',
+                  'float',
+                  'float',
+                  'float',
+                  'S8',
+                  'float']
+    formatsMains = formatsCircuit[0:-1]    
+    typeCircuit = np.dtype({'names':namesCircuit, 'formats':formatsCircuit})
+    typeMains = np.dtype({'names':namesMains, 'formats':formatsMains})
+
+    if '200' in circuit:
+        type = typeMains
+    else:
+        type = typeCircuit
+    
+    currentDatetime = beginDatetime
+    data = []
+    while currentDatetime != endDatetime:
+        # construct path for certain hour of data
+        path  = str(currentDatetime.year) + '/' 
+        path += str(currentDatetime.month) + '/'
+        path += str(currentDatetime.day) + '/'
+        path += str(currentDatetime.hour) 
+        path = '%02d/%02d/%02d/%02d/' % (currentDatetime.year,
+                              currentDatetime.month,
+                              currentDatetime.day,
+                              currentDatetime.hour)
+        filename = '192_168_1_' + circuit + '.log'
+        
+        file = dataDirectory + path + filename
+
+        if verbose == 1:
+            print 'reading ' + file
+
+        # read log file            
+        if os.path.isfile(file):
+            newData = np.loadtxt(file, delimiter=',', dtype = type, skiprows = 1)
+            #print newData
+            if newData.shape != ():
+                if data == []:
+                    data = newData
+                else:
+                    data = np.append(data, newData, axis=0)
+        
+        # increment by one hour
+        currentDatetime = currentDatetime + datetime.timedelta(hours=1)
+        
+    return data
+    
 def getData(plotCircuit, plotDate, downsample, dataDirectory):
     if verbose == 1:
         print 'getting data for', plotCircuit, plotDate
