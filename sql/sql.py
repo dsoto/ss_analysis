0a72eee2a07842db19c327acfdb4d8643da037db
Daniel Soto <danielrsoto@gmail.com>
2011-04-21 16:51:16 -0400

    working cut of using sqlalchemy to get the database primary log data

---
+++ sql/sql.py
@@ -0,0 +1,184 @@
+import sqlalchemy
+import urllib
+
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import DateTime
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy import Float
+from sqlalchemy import Boolean
+from sqlalchemy import Numeric
+from sqlalchemy import Unicode
+
+engine = sqlalchemy.create_engine('postgres://postgres:postgres@localhost:5432/gateway')
+connection = engine.connect()
+
+
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+from sqlalchemy.orm import sessionmaker, relation
+
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+class Meter(Base):
+    """
+    A class that repsents a meter in the gateway
+    """
+    __tablename__ = 'meter'
+
+    id = Column(Integer, primary_key=True)
+    uuid = Column(String)
+    name = Column(String)
+    phone = Column(String)
+    location = Column(String)
+    status = Column(Boolean)
+    date = Column(DateTime)
+    battery = Column(Integer)
+    panel_capacity = Column(Integer)
+
+
+    def __init__(self, name=None, phone=None, location=None,
+                 battery=None, status=None, panel_capacity=None,
+                 communication_interface_id=None):
+        self.uuid = str(uuid.uuid4())
+        self.name = name
+        self.phone = phone
+        self.location = location
+        self.date = get_now()
+        self.battery = battery
+        self.communication_interface_id = communication_interface_id
+        self.panel_capacity = panel_capacity
+
+class Account(Base):
+    """
+    """
+    __tablename__ = "account"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    phone = Column(String)
+    lang = Column(String)
+
+    def __init__(self, name="default", phone=None, lang="en"):
+        self.name = name
+        self.phone = phone
+        self.lang = lang
+
+class Circuit(Base):
+    """
+    """
+    __tablename__ = "circuit"
+
+    id = Column(Integer, primary_key=True)
+    uuid = Column(String)
+    date = Column(DateTime)
+    pin = Column(String)
+    meter_id = Column("meter", ForeignKey("meter.id"))
+    meter = relation(Meter,
+                      lazy=False, primaryjoin=meter_id == Meter.id)
+    energy_max = Column(Float)
+    power_max = Column(Float)
+    status = Column(Integer)
+    ip_address = Column(String)
+    credit = Column(Float)
+    account_id = Column(Integer, ForeignKey('account.id'))
+    account = relation(Account, lazy=False,
+                        cascade="all,delete",
+                        backref='circuit',
+                        primaryjoin=account_id == Account.id)
+
+    def __init__(self, meter=None, account=None,
+                 energy_max=None, power_max=None,
+                 ip_address=None, status=1, credit=0):
+        self.date = get_now()
+        self.uuid = str(uuid.uuid4())
+        self.pin = self.get_pin()
+        self.meter = meter
+        self.energy_max = energy_max
+        self.power_max = power_max
+        self.ip_address = ip_address
+        self.status = status
+        self.credit = credit
+        self.account = account
+
+
+class Log(Base):
+    __tablename__ = "log"
+    id = Column(Integer, primary_key=True)
+    date = Column(DateTime)
+    uuid = Column(String)
+    _type = Column('type', String(50))
+    __mapper_args__ = {'polymorphic_on': _type}
+    circuit_id = Column(Integer, ForeignKey('circuit.id'))
+    circuit = relation(Circuit, lazy=False,
+                       primaryjoin=circuit_id == Circuit.id)
+
+    def __init__(self, date=None, circuit=None):
+        self.date = date
+        self.uuid = str(uuid.uuid4())
+        self.circuit = circuit
+
+
+
+class PrimaryLog(Log):
+    __tablename__ = "primary_log"
+    __mapper_args__ = {'polymorphic_identity': 'primary_log'}
+    id = Column(Integer, ForeignKey('log.id'), primary_key=True)
+    watthours = Column(Float)
+    use_time = Column(Float)
+    status = Column(Integer)
+    created = Column(DateTime)
+    credit = Column(Float, nullable=True)
+    status = Column(Integer)
+
+    def __init__(self, date=None, circuit=None, watthours=None,
+                 use_time=None, status=None, credit=0):
+        Log.__init__(self, date, circuit)
+        self.circuit = circuit
+        self.watthours = watthours
+        self.use_time = use_time
+        self.credit = credit
+        self.created = get_now()
+        self.status = status
+
+    def getUrl(self):
+        return ""
+
+    def getType(self):
+        if self.circuit.ip_address == '192.168.1.200':
+            return 'MAIN'
+        else:
+            return 'CIRCUIT'
+
+    def getCreditAndType(self):
+        if self.getType() == 'MAIN':
+            return [('ct', self.getType()), ('cr', 0)]
+        else:
+            return [('cr', float(self.credit)), ('ct', self.getType())]
+
+    def __str__(self):
+        return urllib.urlencode([('job', 'pp'),
+                                 ('status', self.status),
+                                 ('ts', self.created.strftime("%Y%m%d%H")),
+                                 ('cid', self.circuit.ip_address),
+                                 ('tu', int(self.use_time)),
+                                 ('wh', float(self.watthours))] + self.getCreditAndType())
+
+
+
+import datetime as dt
+
+circuit = session.query(Circuit).get(13)
+
+thisQuery = session.query(PrimaryLog).filter_by(circuit=circuit)
+thisQuery = thisQuery.filter(PrimaryLog.created > dt.datetime(2011,4,9))
+thisQuery = thisQuery.filter(PrimaryLog.created < dt.datetime(2011,4,9,12))
+
+
+for tl in thisQuery:
+    print tl.created
+
+
