ed1c2719d66b652fa0f6b1382099317c84d2eec3
Daniel Soto <danielrsoto@gmail.com>
2011-06-02 11:01:25 -0400

    adds function to look for circuits that are delivering electricity (non-zero watthours) at the same time that they have zero credit and prints a report to the console

--- sql/sql.py
+++ sql/sql.py
@@ -878,6 +878,33 @@ def calculateTimeWithCredit(meter_id,
         print ('%0.2f' % timeWithCredit).rjust(6),
     print
 
+def lookForBadSC20(circuit_id,
+                   dateStart=dt.datetime(2011,5,1),
+                   dateEnd = dt.datetime(2011,6,1)):
+    # get all primary parameters in date range
+    # loop over primary parameters and look for condition watthours>0 and credit<=0
+    # if true print date and circuit_id to console
+    logs = session.query(PrimaryLog)\
+                  .filter(PrimaryLog.circuit_id == circuit_id)\
+                  .filter(PrimaryLog.date > dateStart)\
+                  .filter(PrimaryLog.date <= dateEnd)\
+                  .order_by(PrimaryLog.created)
+    logList = []
+    for log in logs:
+        if log.watthours > 0 and log.credit <= 0:
+            logList.append((log.date, log.circuit_id, log.watthours))
+    logList = list(set(logList))
+    logList.sort()
+
+    cw = 20
+    print 'date'.rjust(cw),
+    print 'circuit id'.rjust(cw),
+    print 'watthours'.rjust(cw)
+    for log in logList:
+        for l in log:
+            print str(l).rjust(cw),
+        print
+
 
 # deprecated or unpopular functions
 
