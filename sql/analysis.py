65e9c089c58c42d73781bfadf51dbe0d6c17f5eb
Daniel Soto <danielrsoto@gmail.com>
2011-06-27 20:13:53 -0400

    moving function from analysis.py to daniel.py

--- sql/analysis.py
+++ sql/analysis.py
@@ -953,36 +953,6 @@ def generate_ictd_figures():
     print 'generating energy histogram'
     plotScatterCreditConsumedVsTimeWithCreditForCircuitList(ml06, may_15, jun_15, plotFileName='ictd/scatterCreditHistogram.pdf')
 
-
-def lookForBadSC20(circuit_id,
-                   dateStart=dt.datetime(2011,5,1),
-                   dateEnd = dt.datetime(2011,6,1)):
-    # get all primary parameters in date range
-    # loop over primary parameters and look for condition watthours>0 and credit<=0
-    # if true print date and circuit_id to console
-    logs = session.query(PrimaryLog)\
-                  .filter(PrimaryLog.circuit_id == circuit_id)\
-                  .filter(PrimaryLog.date > dateStart)\
-                  .filter(PrimaryLog.date <= dateEnd)\
-                  .order_by(PrimaryLog.created)
-    logList = []
-    for log in logs:
-        if log.watthours > 0 and log.status == 0:
-            logList.append((log.date, log.circuit_id, log.watthours, log.status, log.credit))
-    logList = list(set(logList))
-    logList.sort()
-
-    cw = 20
-    print 'date'.rjust(cw),
-    print 'circuit id'.rjust(cw),
-    print 'watthours'.rjust(cw),
-    print 'status'.rjust(cw),
-    print 'credit'.rjust(cw)
-    for log in logList:
-        for l in log:
-            print str(l).rjust(cw),
-        print
-
 def calculateCreditConsumedForCircuit(circuit_id,
                             dateStart=dateStart,
                             dateEnd=dateEnd,
--- sql/daniel.py
+++ sql/daniel.py
@@ -7,6 +7,34 @@ def calculateEuclidianDistance(powerVector1, powerVector2):
     return sum((powerVector2 - powerVector1)**2)**(0.5)
 
 
+def lookForBadSC20(circuit_id,
+                   dateStart=dt.datetime(2011,5,1),
+                   dateEnd = dt.datetime(2011,6,1)):
+    # get all primary parameters in date range
+    # loop over primary parameters and look for condition watthours>0 and credit<=0
+    # if true print date and circuit_id to console
+    logs = session.query(PrimaryLog)\
+                  .filter(PrimaryLog.circuit_id == circuit_id)\
+                  .filter(PrimaryLog.date > dateStart)\
+                  .filter(PrimaryLog.date <= dateEnd)\
+                  .order_by(PrimaryLog.created)
+    logList = []
+    for log in logs:
+        if log.watthours > 0 and log.status == 0:
+            logList.append((log.date, log.circuit_id, log.watthours, log.status, log.credit))
+    logList = list(set(logList))
+    logList.sort()
+
+    cw = 20
+    print 'date'.rjust(cw),
+    print 'circuit id'.rjust(cw),
+    print 'watthours'.rjust(cw),
+    print 'status'.rjust(cw),
+    print 'credit'.rjust(cw)
+    for log in logList:
+        for l in log:
+            print str(l).rjust(cw),
+        print
 
 cid = 25
 
