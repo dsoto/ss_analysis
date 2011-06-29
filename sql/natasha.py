3cbb8176e56bb84481b0068b30b038f8dffdcc06
no140 <no140@columbia.edu>
2011-06-29 17:47:44 -0400

    new functions all at once sorry but gitk is a pain

--- sql/natasha.py
+++ sql/natasha.py
@@ -2,6 +2,18 @@ from sql.analysis import *
 
 import datetime as dt
 
+#font properties---------------------
+import matplotlib.font_manager as mpf
+#for labels
+labelFont = mpf.FontProperties()
+labelFont.set_family('serif')
+labelFont.set_size(14)
+#labelFont.set_linespacing(1.5)
+#font prop's for annotation
+textFont = mpf.FontProperties()
+textFont.set_family('monospace')
+textFont.set_size(6)
+
 def writeMessageRateOfMeters(dateStart=dt.datetime(2011,6,24), dateEnd=today, fullWrite=True):
     '''
     if difference(circuit_id_list.sort, mali001) == 0:
@@ -54,6 +66,52 @@ def writeMessageRateOfMeters(dateStart=dt.datetime(2011,6,24), dateEnd=today, fu
                 f.write(str(sum(avg_mrate)/len(avg_mrate)));f.write('\n')
             currentDate += dt.timedelta(days=1)
     f.close()
+
+def plotMessageRateOfMeters(dateStart=dt.datetime(2011,6,24), dateEnd=today):
+    meters = [4,6,7,8]
+    currentDate=dateStart
+    dateList = []
+    d = dateStart
+    while d < dateEnd:  #-dt.timedelta(days=1):
+        dateList.append(d)
+        d += dt.timedelta(days=1)
+    mrate=[[]*len(meters) for x in xrange(len(meters))]
+    while currentDate < dateEnd:
+        for m in range(len(meters)):
+            cct_mrate =[]
+            circuit_id_list = getCircuitsForMeter(meters[m])
+            #avg_mrate=[]
+            for i,c in enumerate(circuit_id_list):
+                dates, data = getDataListForCircuit(c, currentDate, currentDate+dt.timedelta(days=1))
+                cct_mrate.append(len(dates)/24.0)
+            mrate[m].append(sum(cct_mrate)/len(cct_mrate))
+        currentDate += dt.timedelta(days=1)
+    print mrate
+    fig = plt.figure()
+    ax = fig.add_axes((.1,.3,.8,.6))
+    for m in range(len(meters)):
+        ax.plot(dateList, mrate[m], 'x-', label=getMeterName(meters[m]))
+        #ax.legend(getMeterName(meters[m]), loc=(0.9,0.9-(0.1*m)))
+        #fig.text(0.85,(0.01+0.05*m),getMeterName(meters[m]))
+    ax.legend(loc=0)
+    #fig.legend()
+    titleString = 'meters-messages received ' + dateStart.date().__str__() + '_to_' + dateEnd.date().__str__()
+    ax.set_title(titleString)
+    ax.set_ylabel("% messages received")
+    ax.set_xlabel("Date")
+    annotation = []
+    annotation.append('plot generated ' + today.__str__() )
+    annotation.append('function = ' + plotMessageRateOfMeters.__name__)
+    annotation.append('meters = ' + str(meters))
+    annotation.append('date start = ' + str(dateStart))
+    annotation.append('date end = ' + str(dateEnd))
+    annotation = '\n'.join(annotation)
+    #plt.show()
+    fig.text(0.01,0.01, annotation) #, fontproperties=textFont)
+    plotFileName = titleString + '.pdf'
+    fig.savefig(plotFileName, transparent=True)
+
+#plotMessageRateOfMeters()
 
 def printEnergyUsedPerDayByCircuitsOnMeter(circuit_id_list, dateStart=dateStart, dateEnd=dateEnd):
     dataList = np.array([])
@@ -135,3 +193,138 @@ def specificTimeIssues(circuit_id_list=[81,82,84,89,90], dateStart=dt.datetime(2
                 f.write('\n')
         currentDate += dt.timedelta(days=1)
     f.close()
+
+
+def plotCreditDiffs(meter_id, dateStart=dt.datetime(2011,5,13),
+                        dateEnd=dt.datetime(2011,5,20),
+                            verbose = 0, introspect=False, showMains=False):
+
+    circuits = getCircuitsForMeter(meter_id)
+    dtSt = str.replace(str(dateStart), ' 00:00:00', '')
+    dtEnd = str.replace(str(dateEnd), ' 00:00:00', '')
+
+    #get date labels
+    num_days = dateEnd - dateStart
+    dateList = []
+    d = dateStart
+    while d <= dateEnd:
+        dateList.append(d)
+        d += dt.timedelta(days=1)
+    #print dateList
+    yLabels = np.arange(0,1100,200)
+
+    # drop mains circuit
+    if showMains == False:
+        for c in circuits:
+            if session.query(Circuit).filter(Circuit.id == c)[0].ip_address == '192.168.1.200':
+                circuits.remove(c)
+
+    fig = plt.figure()
+
+    # create figure and axes with subplots
+    if len(circuits) > 12:
+        numPlotsX = 4
+        numPlotsY = 5
+    else:
+        numPlotsX = 4
+        numPlotsY = 3
+
+    for i,c in enumerate(circuits):
+        calculatedCredits = calculateCreditJumps(c, dateStart, dateEnd, verbose)
+        loggedPurchases = calculateCreditPurchase(c, dateStart, dateEnd, verbose)
+
+        dates = np.union1d(calculatedCredits[0], loggedPurchases[0])
+        print dates
+        dates = matplotlib.dates.date2num(dates)
+        print dates
+        #dates = list(set(dates))
+        #dates.sort()
+        calculatedCreditDates = matplotlib.dates.date2num(calculatedCredits[0])
+        mask1 = []
+        for l in range(len(calculatedCreditDates)):
+            ind = np.where(dates==calculatedCreditDates[l])
+            #print ind
+            mask1.append(int(ind[0]))       #because ind was an array of arrays!
+        print mask1
+        #mask1 = np.where(dates,calculatedCredits[0])
+        #mask1 = np.setmember1d(dates, calculatedCredits[0])    #mergesort not avail for type
+        data1 = [-100]*len(dates)
+        for k in range(len(mask1)):
+            data1[mask1[k]] = calculatedCredits[1][k]
+        #data1[np.invert(mask1)] = 0
+        print data1
+        #mask2 = np.in1d(dates,loggedPurchases[0])
+        #mask2 = dates == loggedPurchases[0]
+        #mask2 = np.setmember1d(dates, loggedPurchases[0])  #mergesort not avail for type
+        purchaseDates = matplotlib.dates.date2num(loggedPurchases[0])
+        mask2 = []
+        for l in range(len(purchaseDates)):
+            ind = np.where(dates==purchaseDates[l])
+            #print ind
+            mask2.append(int(ind[0]))
+        data2 = [-100]*len(dates)
+        for j in range(len(mask2)):
+            data2[mask2[j]] = loggedPurchases[1][j]
+        #data2[mask2] = loggedPurchases[1]
+        #data2[np.invert(mask2)] = 0
+        print data2
+        '''
+        dates, data = getDataListForCircuit(c, dateStart,dateEnd,quantity='credit')
+        dates = matplotlib.dates.date2num(dates)
+        data1 = calculatedCredits[1]
+        data2 = loggedPurchases[1]
+        '''
+        # how to make xlabels & ylabels smaller?
+        # how to make room for xlabels on all graphs?
+        thisAxes = fig.add_subplot(numPlotsX, numPlotsY, i+1, xlim=(dateStart, dateEnd), ylim=(0,1100)) #to keep all plots even, assuming 1100 is enough
+        thisAxes.plot_date(dates, data1, ls=' ', ms=7, marker='o', c='b')
+        thisAxes.plot_date(dates, data2, ls=' ', ms=12, marker='x', c='r')
+        #from matplotlib import font_manager
+        #smallsize = font_manager.FontProperties(size=mpl.rcParams['font.size'])
+        thisAxes.set_xticklabels(dateList, fontproperties=textFont)
+        thisAxes.set_yticklabels(yLabels, fontproperties=textFont)
+        #thisAxes.xlim(xmin=1)
+        thisAxes.text(0.7,0.7,str(c),size="x-small", transform = thisAxes.transAxes)
+
+
+    fileNameString = 'credit diffs on meter ' +  ' ' + str(meter_id) + '-' + dtSt + 'to' + dtEnd + '.pdf'
+    fig.suptitle(fileNameString)
+    fig.autofmt_xdate()
+    if introspect:
+        plt.show()
+    fig.savefig(fileNameString)
+
+
+def plotCctsWithWatthourDrops(meters=[4,6,7,8], dateStart=dt.datetime(2011,6,24), dateEnd=dt.datetime(2011,6,28),
+                          verbose=0):
+
+    #meters = [4,6,7,8]
+    circuit_id_list=[]
+    for m in range(len(meters)):
+        ccts = getCircuitsForMeter(meters[m])
+        #print ccts
+        for x in range(len(ccts)):
+            circuit_id_list.append(ccts[x])
+    print circuit_id_list
+    cctdates = [[]*len(circuit_id_list) for x in xrange(len(circuit_id_list))]
+    # break up time period into days
+    num_days = dateEnd - dateStart
+    beg_day = dateStart
+    print len(circuit_id_list)
+    for k in range(num_days.days):
+        for i,c in enumerate(circuit_id_list):
+            print c
+            errortimes = inspectDayOfWatthours(c, beg_day)
+            if len(errortimes)>0:
+                #remove times from dates
+                errordates=[]
+                for k in range(len(errortimes)):
+                    #errordates = errortimes[i].strftime('%Y,%m,%d')
+                    #errordates[k] = dt.datetime(....)
+                    cctdates[i].append(dt.datetime(errortimes[k].year, errortimes[k].month, errortimes[k].day))
+        beg_day = beg_day + dt.timedelta(days=1)
+    print cctdates
+    for i,c in enumerate(circuit_id_list):
+        if cctdates[i]:                 #if there are entries for that cct
+            for k in range(len(cctdates[i])):
+                plotDatasForCircuit(c, cctdates[i][k], cctdates[i][k] + dt.timedelta(days=1), titleString='whdrops/cct '+str(c)+'on'+str(cctdates[i][k].date()))
