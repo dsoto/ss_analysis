afd2db494e0167a08ed02dcd4bab3646be2df701
Daniel Soto <danielrsoto@gmail.com>
2011-07-25 14:09:08 -0400

    adds logging messages to sql/analysis.py

--- sql/analysis.py
+++ sql/analysis.py
@@ -505,7 +505,9 @@ def getDataListForCircuit(circuit_id,
         data - list of reported data
     '''
     # get numpy arrays of dates, timestamps, and data
-    tw.log.info('running getDataListForCircuit on ' + str(circuit_id) + ' for quantity = ' + quantity)
+    tw.log.info('->->-> getDataListForCircuit')
+    tw.log.info('       circuit  = ' + str(circuit_id))
+    tw.log.info('       quantity = ' + quantity)
     dates, created, data = getRawDataListForCircuit(circuit_id,
                                                     dateStart,
                                                     dateEnd,
@@ -526,9 +528,11 @@ def getDataListForCircuit(circuit_id,
             mask.append(True)
         else:
             mask.append(False)
-            tw.log.info('removing sample at ' + str(dates[i]) + ' from the future')
+            #tw.log.info('removing sample at ' + str(dates[i]) + ' from the future')
+
 
     mask = np.array(mask)
+    tw.log.info('removing ' + str(len(np.extract(mask==False,mask))) + ' samples from the future')
 
     dates = dates[mask]
     data = data[mask]
@@ -537,7 +541,11 @@ def getDataListForCircuit(circuit_id,
     dataList = []
     for i in range(len(dates)):
         dataList.append((dates[i], data[i]))
+    num_raw_samples = len(dataList)
+    tw.log.info('number of raw samples    = ' + str(num_raw_samples))
     dataList = list(set(dataList))
+    num_unique_samples = len(dataList)
+    tw.log.info('number of unique samples = ' + str(num_unique_samples))
     dataList.sort()
     dates = [d[0] for d in dataList]
     data = [d[1] for d in dataList]
@@ -545,7 +553,7 @@ def getDataListForCircuit(circuit_id,
     if verbose > 0:
         for d in dataList:
             tw.log.info(str(d))
-
+    tw.log.info('<-<-<- getDataListForCircuit')
     return dates, data
 
 def getRawDataListForCircuit(circuit_id,
@@ -794,7 +802,8 @@ def plotEnergyHistogram(circuit_id_list,
     # range depends on data
     if bins == None:
         high = int(np.ceil(max(dataList)) + 5)
-        bins = [0,1] + range(5,high,5)
+        #bins = [0,1] + range(5,high,5)
+        bins = range(0, high, 5)
     ax.hist(dataList, bins=bins, normed=False, facecolor='#dddddd')
     ax.set_xlabel("Daily Watthours")    #, fontproperties=labelFont)
     ax.set_ylabel("Days of Usage")  #, fontproperties=labelFont)
@@ -804,7 +813,10 @@ def plotEnergyHistogram(circuit_id_list,
     annotation.append('circuits = ' + str(circuit_id_list))
     annotation.append('date start = ' + str(dateStart))
     annotation.append('date end = ' + str(dateEnd))
+    for ann in annotation:
+        tw.log.info(ann)
     annotation = '\n'.join(annotation)
+
     #plt.show()
     fig.text(0.01,0.01, annotation) #, fontproperties=textFont)
     fig.savefig(plotFileName, transparent=True)
@@ -815,7 +827,9 @@ def getEnergyForCircuitForDayByMax(circuit_id,
     needs to check for sufficient number of data samples
     needs to check for watthour drops
     '''
-    tw.log.info('entering getEnergyForCircuitForDayByMax for circuit ' + str(circuit_id))
+    tw.log.info('->->-> getEnergyForCircuitForDayByMax')
+    tw.log.info('       circuit = ' + str(circuit_id))
+    tw.log.info('          date = ' + str(day))
     dates, watthours = getDataListForCircuit(circuit_id,
                                              day,
                                              day+dt.timedelta(days=1),
@@ -828,9 +842,11 @@ def getEnergyForCircuitForDayByMax(circuit_id,
     power = np.diff(watthours)
     num_decreases = len(np.extract(power < 0, power))
     tw.log.info('samples of watthour data = ' + str(len(watthours)))
+    if len(watthours) > 24:
+        tw.log.error('too many watthour samples for one day')
     tw.log.info('number of watthour drops = ' + str(num_decreases))
     tw.log.info('reporting ' + str(energy) + ' watthours')
-    tw.log.info('-------')
+    tw.log.info('<-<-<- getEnergyForCircuitForDayByMax')
     return energy, len(watthours), num_decreases
 
 def calculateAverageEnergyForCircuit(circuit_id=70,
@@ -876,14 +892,24 @@ def calculateAverageEnergyForCircuit(circuit_id=70,
 def getEnergyForCircuit(circuit_id,
                         dateStart=dateStart,
                         dateEnd=dateEnd):
+    '''
+    gets energy for circuit over date range specified by dateStart and dateEnd
+    if no data, does not append to list
+    '''
+    tw.log.info('->->-> getEnergyForCircuit')
     data = []
     dates = []
     currentDate = dateStart
     while currentDate < dateEnd:
-        data.append(getEnergyForCircuitForDayByMax(circuit_id, currentDate)[0])
-        dates.append(currentDate)
+        result = getEnergyForCircuitForDayByMax(circuit_id, currentDate)
+        if result[1] >= 24 and result[0] != 0.0:
+            data.append(result[0])
+            dates.append(currentDate)
+        else:
+            tw.log.info('rejected data for ' + str(currentDate))
         currentDate += dt.timedelta(days=1)
     data = np.array(data)
+    tw.log.info('<-<-<- getEnergyForCircuit')
     return data, dates
 
 def energyTest(circuit_id_list, dateStart=dt.datetime(2011,6,5),
