de420f68c67abe1cfb66b1e089a0e4754871bb81
no140 <no140@columbia.edu>
2011-07-27 14:15:05 -0400

    plotPowerConsumptionError function

--- sql/natasha.py
+++ sql/natasha.py
@@ -651,6 +651,7 @@ def plotCreditDiffs(meter_id, dateStart=dt.datetime(2011,5,13),
     fig.savefig(fileNameString)
 
 
+
 def plotCctsWithWatthourDrops(meters=[4,6,7,8], dateStart=dt.datetime(2011,6,24), dateEnd=dt.datetime(2011,6,28),
                           verbose=0):
 
@@ -1010,3 +1011,45 @@ def plotHourlySelfConsumption(meter_id=8,
     fig.text(0.01,0.01, annotation) #, fontproperties=textFont)
     fig.savefig(fileNameString+'.pdf')
     tw.log.info('exiting plotHourlySelfConsumption')
+
+def plotPowerConsumptionError():
+    R = [4980, 9950, 19900, 14200, 2490, 994, 500]
+    R = np.flipud(np.sort(R))
+    Vrms = 125.0
+    pred_power = [np.square(Vrms)/R[x] for x in range(len(R))]
+    print pred_power
+    belkin = [3.2, 1.6, 0.8, 1.1, 6.3, 15.6, 31.0]
+    belkin = np.sort(belkin)
+    sc20_1 = [2.3, 0.8, 0, 0, 5.5, 14.8, 30.4]
+    sc20_2 = [2.7, 1.1, 0, 0, 5.8, 15.2, 30.8]
+    sc20_1 = np.sort(sc20_1)
+    sc20_2 = np.sort(sc20_2)
+    mains_measured = [31.0, 29.4, 28.6, 29, 34.1, 43.6, 59.5]
+    mains_measured = np.sort(mains_measured)
+    baseline = np.ones(len(R)) * 27.9
+    sc20_mains = mains_measured - baseline
+
+    fig = plt.figure()
+    ax = fig.add_axes((.1,.3,.8,.6))
+    ax.loglog(pred_power, sc20_mains, 'x-', color='r', label='Mains')
+    ax.loglog(pred_power, pred_power, '-', color='k', label='ideal')
+    ax.loglog(pred_power, [pred_power[x]*.9 for x in range(len(pred_power))], '-', color='0.75', label='-10%')
+    ax.loglog(pred_power, [pred_power[x]*1.1 for x in range(len(pred_power))], '-', color='0.75', label='+10%')
+    ax.loglog(pred_power, sc20_1, '^-', color='b', label='sc20_1')
+    ax.loglog(pred_power, sc20_2, '^-', color='g', label='sc20_2')
+    ax.legend(loc=0)
+    ax.set_ylim((0,35))
+    ax.set_xlim((0,35))
+    ax.set_xlabel('predicted power (watts)')
+    ax.set_ylabel('measured power (watts)')
+    #ax.set_xticklabels(np.exp(
+    fileNameString = 'log-log plot of power measurement error'
+    ax.set_title(fileNameString)
+    annotation = []
+    annotation.append('plot generated ' + today.__str__() )
+    annotation.append('function = ' + plotPowerConsumptionError.__name__)
+    annotation.append('data points at predicted power of: ' + str(np.round(pred_power,2)))
+    annotation = '\n'.join(annotation)
+    #plt.show()
+    fig.text(0.01,0.01, annotation) #, fontproperties=textFont)
+    fig.savefig(fileNameString+'.pdf')
